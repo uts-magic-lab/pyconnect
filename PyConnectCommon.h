05c54d57923a4c4460f0b1543d34ac4dcdcd5eb6
Xun Wang <wang.xun@gmail.com>
2019-08-21 17:48:29 +1000

    WIP.

--- PyConnectCommon.h
+++ PyConnectCommon.h
@@ -247,6 +247,54 @@ template <> const char * getVarTypeName( const std::string& )
 {
   return "string";
 }
+
+template<class F>
+struct function_traits;
+ 
+// function pointer
+template<class R, class... Args>
+struct function_traits<R(*)(Args...)> : public function_traits<R(Args...)>
+{};
+ 
+template<class R, class... Args>
+struct function_traits<R(Args...)>
+{
+  using return_type = R;
+
+  static constexpr std::size_t arity = sizeof...(Args);
+
+  template <std::size_t N>
+  struct argument
+  {
+    static_assert(N < arity, "error: invalid parameter index.");
+    using type = typename std::tuple_element<N,std::tuple<Args...>>::type;
+  };
+};
+
+// member function pointer
+template<class C, class R, class... Args>
+struct function_traits<R(C::*)(Args...)> : public function_traits<R(C&,Args...)>
+{};
+
+// const member function pointer
+template<class C, class R, class... Args>
+struct function_traits<R(C::*)(Args...) const> : public function_traits<R(C&,Args...)>
+{};
+
+template<class C, class R, class... Args>
+struct function_traits<R(C&,Args...)>
+{
+  using return_type = R;
+
+  static constexpr std::size_t arity = sizeof...(Args);
+
+  template <std::size_t N>
+  struct argument
+  {
+    static_assert(N < arity, "error: invalid parameter index.");
+    using type = typename std::tuple_element<N,std::tuple<Args...>>::type;
+  };
+};
 #endif
 
 template <typename DataType> static int packToLENumber( const DataType & v, unsigned char * & dataPtr )
